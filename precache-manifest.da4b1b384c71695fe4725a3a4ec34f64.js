0a94eaf8b681622691a9e7c66cd63b0564d5cb67
themiee <navyblack16@yahoo.com>
2020-04-28 07:23:35 +0100

    Updates

---
+++ precache-manifest.da4b1b384c71695fe4725a3a4ec34f64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fe40bfb4e73c31ab18d91d5a7000928",
+    "url": "/TodoList-React/index.html"
+  },
+  {
+    "revision": "ad46df5f6aaaf6ea21aa",
+    "url": "/TodoList-React/static/css/main.8423b534.chunk.css"
+  },
+  {
+    "revision": "440521fafab6d4ba00c8",
+    "url": "/TodoList-React/static/js/2.dcf55f31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoList-React/static/js/2.dcf55f31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad46df5f6aaaf6ea21aa",
+    "url": "/TodoList-React/static/js/main.446f8440.chunk.js"
+  },
+  {
+    "revision": "d039ec541149dd9e9584",
+    "url": "/TodoList-React/static/js/runtime-main.d446d6ec.js"
+  }
+]);
